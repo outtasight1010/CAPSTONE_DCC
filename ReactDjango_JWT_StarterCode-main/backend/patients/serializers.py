fc48a99e26ff2d900b8a680c07efe49cd7391df6
Michelle Lamelza <mishlamelza@gmail.com>
2023-08-27 01:46:21 -0400

    applied migrations to patients app

---
+++ ReactDjango_JWT_StarterCode-main/backend/patients/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Patient
+
+class PatientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Patient
+        fields = [
+            'id', 'first_name', 'last_name', 'contact_number', 
+            'medical_history', 'joined_queue', 'queue_position', 
+            'estimated_wait_time', 'user'
+        ]
