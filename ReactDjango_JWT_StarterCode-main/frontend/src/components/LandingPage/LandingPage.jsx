9b95537cb20870a034b33dce59becd2696a1dd07
Michelle Lamelza <mishlamelza@gmail.com>
2023-08-29 01:30:18 -0400

    created a LandingPage (aka default page)

---
+++ ReactDjango_JWT_StarterCode-main/frontend/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <h2>Welcome to Our Medical Center</h2>
+      <div className="image-container">
+        <img src= 'https://www.thehealthjournals.com/wp-content/uploads/2019/07/iStock-998339320.jpg' alt="Medical Center" />
+        
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
