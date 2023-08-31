c1d8782245f12b3fd29c145f22b17c387e492066
Michelle Lamelza <mishlamelza@gmail.com>
2023-08-31 02:31:12 -0400

    Update InsurancePage.js

--- ReactDjango_JWT_StarterCode-main/frontend/src/pages/InsurancePage/InsurancePage.js
+++ ReactDjango_JWT_StarterCode-main/frontend/src/pages/InsurancePage/InsurancePage.js
@@ -1,23 +1,57 @@
 import React, { useState } from "react";
 
 const InsurancePage = () => {
-  const [insuranceData, setInsuranceData] = useState({
-    insuranceProvider: "",
-    policyNumber: "",
-    // Other insurance fields
-  });
+  const [insuranceName, setInsuranceName] = useState("");
+  const [policyNumber, setPolicyNumber] = useState("");
+  const [noInsurance, setNoInsurance] = useState(false);
 
-  const handleInsuranceSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle insurance submission here
+    // Handle form submission here, e.g. send data to the backend
+    console.log("Insurance Name:", insuranceName);
+    console.log("Policy Number:", policyNumber);
+    console.log("No Insurance:", noInsurance);
+    // Reset form fields after submission
+    setInsuranceName("");
+    setPolicyNumber("");
+    setNoInsurance(false);
   };
 
   return (
-    <div>
+    <div className="container">
       <h2>Add Insurance Credentials</h2>
-      <form onSubmit={handleInsuranceSubmit}>
-        {/* Insurance form fields */}
-        <button type="submit">Submit Insurance</button>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="insuranceName">Insurance Name:</label>
+          <input
+            type="text"
+            id="insuranceName"
+            value={insuranceName}
+            onChange={(e) => setInsuranceName(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <div>
+          <label htmlFor="policyNumber">Policy Number:</label>
+          <input
+            type="text"
+            id="policyNumber"
+            value={policyNumber}
+            onChange={(e) => setPolicyNumber(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={noInsurance}
+              onChange={(e) => setNoInsurance(e.target.checked)}
+            />
+            No Insurance
+          </label>
+        </div>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
