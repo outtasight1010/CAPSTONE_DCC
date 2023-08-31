df4fcd2d1ebdd60d5669cd3ae9aa10ddf48ac2c1
Michelle Lamelza <mishlamelza@gmail.com>
2023-08-31 00:56:56 -0400

    added background image + updated css to LandingPage

--- ReactDjango_JWT_StarterCode-main/frontend/src/components/LandingPage/LandingPage.jsx
+++ ReactDjango_JWT_StarterCode-main/frontend/src/components/LandingPage/LandingPage.jsx
@@ -3,20 +3,21 @@ import './LandingPage.css';
 
 const LandingPage = () => {
   return (
-    <div className="landing-page">
+    <div className="landing-page background-image"> {/* Add the background-image class here */}
       <h2 className="landing-title">ER Excellence: Treating Urgencies, & Valuing YOUR Time</h2>
       <div className="image-container">
-  <a href="#" className="image-link">
-    <img
-      src="https://www.thehealthjournals.com/wp-content/uploads/2019/07/iStock-998339320.jpg"
-      alt="Medical Center"
-      className="landing-image"
-    />
-  </a>
+        <a href="#" className="image-link">
+          <img
+            src="https://www.thehealthjournals.com/wp-content/uploads/2019/07/iStock-998339320.jpg"
+            alt="Medical Center"
+            className="landing-image"
+          />
+        </a>
       </div>
     </div>
   )
 };
 
 export default LandingPage;
+
 
