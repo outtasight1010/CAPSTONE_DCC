c9fa4d2f51b5dc9153e8511bfddd0be8315f6184
Michelle Lamelza <mishlamelza@gmail.com>
2023-09-12 22:38:29 -0400

    created an array of random quotes. Added this to my DashboardPage (just to add a little fun to the page)

--- ReactDjango_JWT_StarterCode-main/frontend/src/pages/DashboardPage/DashboardPage.jsx
+++ ReactDjango_JWT_StarterCode-main/frontend/src/pages/DashboardPage/DashboardPage.jsx
@@ -1,23 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import './DashboardPage.css';
 
 const DashboardPage = () => {
+  const [showQuote, setShowQuote] = useState(false);
+  const [dailyQuote, setDailyQuote] = useState('');
+
+
+  const quotes = [
+    "Never ever ever EVER give up.",
+    "Every passing minute is another chance to turn it all around.",
+    "Let's make it a great day.",
+    "Keep pushing forward.",
+    "You are stronger than you think.",
+    
+  ];
+
+  const getRandomQuote = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    return quotes[randomIndex];
+  };
+
+  const handleDailyQuoteClick = () => {
+    const quote = getRandomQuote();
+    setDailyQuote(quote);
+    setShowQuote(true);
+  };
+
   return (
     <div className="dashboard-container">
       <div className="dashboard-content">
         <h1>Welcome to Your Dashboard</h1>
         <p>What would you like to do?</p>
         <div className="dashboard-actions">
-        <Link to="/select-patient">In Holding</Link>
-          <Link to="/view-queue"> Queue</Link>
+          <Link to="/select-patient">In Holding</Link>
+          <Link to="/view-queue">Queue</Link>
           <Link to="/add-patient">Processed</Link>
-          <Link to="/add-quote">Daily Quote</Link>
+          <button onClick={handleDailyQuoteClick}>Daily Quote</button>
         </div>
+        {showQuote && (
+          <div className="quote-container">
+            <blockquote>
+              <p>{dailyQuote}</p>
+            </blockquote>
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
 export default DashboardPage;
+
