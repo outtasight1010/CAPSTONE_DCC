3194d4df467b123fc39d2fa39329b2c3e0386ad6
Michelle Lamelza <mishlamelza@gmail.com>
2023-09-18 00:02:00 -0400

    added image, and updated css to MedicalSuppliesPage

--- ReactDjango_JWT_StarterCode-main/frontend/src/components/MedicalSupplies/MedicalSupplies.jsx
+++ ReactDjango_JWT_StarterCode-main/frontend/src/components/MedicalSupplies/MedicalSupplies.jsx
@@ -1,21 +1,38 @@
 import React, { useState } from "react";
+import './MedicalSupplies.css';
 
 const MedicalSuppliesPage = () => {
   const [supplyName, setSupplyName] = useState("");
   const [quantity, setQuantity] = useState("");
-  const [supplies, setSupplies] = useState([]); // Local state for supplies
+  const [supplies, setSupplies] = useState([
+    // Initialize with some sample supplies
+    { id: 1, supplyName: "Adhesive bandages", quantity: 10 },
+    { id: 2, supplyName: "Gauze", quantity: 5 },
+    { id: 3, supplyName: "Hypodermic needles", quantity: 8 },
+    { id: 4, supplyName: "Gloves", quantity: 11 },
+    { id: 5, supplyName: "Tweezers", quantity: 12 },
+    { id: 6, supplyName: "Antiseptic wipes", quantity: 14 },
+    { id: 4, supplyName: "Foil blankets", quantity: 4 },
+    { id: 5, supplyName: "Thread", quantity: 19 },
+    { id: 6, supplyName: "Sterilized needles", quantity: 7 }
+  ]);
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
 
-    // Create a new supply object from form data
-    const newSupply = {
-      supplyName,
-      quantity,
-    };
+    // Find the supply to update by its name
+    const updatedSupplies = supplies.map((supply) => {
+      if (supply.supplyName === supplyName) {
+        return {
+          ...supply,
+          quantity: parseInt(quantity), // Update quantity
+        };
+      }
+      return supply;
+    });
 
-    // Update the supplies list with the new supply
-    setSupplies([...supplies, newSupply]);
+    // Update the supplies list with the updated supply
+    setSupplies(updatedSupplies);
 
     // Reset the form fields after submitting
     setSupplyName("");
@@ -23,8 +40,19 @@ const MedicalSuppliesPage = () => {
   };
 
   return (
-    <div>
+    <div className="medical-supplies-container">
       <h2>Medical Supplies</h2>
+      <img
+  src={'https://t4.ftcdn.net/jpg/03/03/86/77/240_F_303867793_FLubzhbNPUPiMdInEOXYW56n1BoZHBjM.jpg'}
+  alt="Medical Supplies"
+  style={{
+    width: '300px', 
+    height: 'auto', 
+    border: '2px solid #000', 
+    borderRadius: '8px', 
+    
+  }}
+/>
       <form onSubmit={handleFormSubmit}>
         <label>
           Supply Name:
@@ -47,20 +75,18 @@ const MedicalSuppliesPage = () => {
         <button type="submit">Update</button>
       </form>
 
-      {/* Display the list of supplies */}
-      <div>
-        <h3>Current Supplies:</h3>
-        <ul>
-          {supplies.map((supply, index) => (
-            <li key={index}>
-              {supply.supplyName} - Quantity: {supply.quantity}
-            </li>
-          ))}
-        </ul>
-      </div>
+      {/* Display supplies vertically */}
+      <ul className="supply-list">
+        {supplies.map((supply, index) => (
+          <li key={index} className="supply-item">
+            {supply.supplyName} - Quantity: {supply.quantity}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
 export default MedicalSuppliesPage;
+
 
