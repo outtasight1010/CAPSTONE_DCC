3a5a487aefe33007b97ad0603f66dad00838e09e
Michelle Lamelza <mishlamelza@gmail.com>
2023-09-19 15:16:21 -0400

    updating urls path for medstaff

--- ReactDjango_JWT_StarterCode-main/backend/medstaff/urls.py
+++ ReactDjango_JWT_StarterCode-main/backend/medstaff/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('medical-staff/', views.list_medical_staff, name='list_medical_staff'),
-    path('medical-staff/create/', views.create_medical_staff, name='create_medical_staff'),
-    path('medical-staff/<int:pk>/', views.update_medical_staff, name='update_medical_staff'),
-    path('medical-staff/<int:pk>/delete/', views.delete_medical_staff, name='delete_medical_staff'),
-    path('staff-on-duty/', views.staff_on_duty, name='staff_on_duty'),# realistically, probabaly the only staff I will use
+    #path('medical-staff/', views.list_medical_staff, name='list_medical_staff'),
+    #path('medical-staff/create/', views.create_medical_staff, name='create_medical_staff'),
+    #path('medical-staff/<int:pk>/', views.update_medical_staff, name='update_medical_staff'),
+    #path('medical-staff/<int:pk>/delete/', views.delete_medical_staff, name='delete_medical_staff'),
+    #path('staff-on-duty/', views.staff_on_duty, name='staff_on_duty'),# realistically, probabaly the only staff I will use
 ]
 
--- ReactDjango_JWT_StarterCode-main/backend/medstaff/views.py
+++ ReactDjango_JWT_StarterCode-main/backend/medstaff/views.py
@@ -5,13 +5,6 @@ from rest_framework.permissions import IsAuthenticated
 from .models import MedicalStaff
 from .serializers import MedicalStaffSerializer
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def list_medical_staff(request):
-    medical_staff = MedicalStaff.objects.all()
-    serializer = MedicalStaffSerializer(medical_staff, many=True)
-    return Response(serializer.data)
-
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
@@ -20,36 +13,3 @@ def staff_on_duty(request):
     return Response(staff)
 
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def create_medical_staff(request):
-    serializer = MedicalStaffSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['PUT'])
-@permission_classes([IsAuthenticated])
-def update_medical_staff(request, pk):
-    try:
-        medical_staff = MedicalStaff.objects.get(pk=pk)
-    except MedicalStaff.DoesNotExist:
-        return Response({"message": "Medical staff not found."}, status=status.HTTP_404_NOT_FOUND)
-
-    serializer = MedicalStaffSerializer(medical_staff, data=request.data, partial=True)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['DELETE'])
-@permission_classes([IsAuthenticated])
-def delete_medical_staff(request, pk):
-    try:
-        medical_staff = MedicalStaff.objects.get(pk=pk)
-    except MedicalStaff.DoesNotExist:
-        return Response({"message": "Medical staff not found."}, status=status.HTTP_404_NOT_FOUND)
-
-    medical_staff.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
