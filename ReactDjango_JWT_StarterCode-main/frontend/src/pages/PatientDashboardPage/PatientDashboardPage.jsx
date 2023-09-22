834f2c7e4a1731186beb5da1d616e73a48196059
Michelle Lamelza <mishlamelza@gmail.com>
2023-09-22 14:19:00 -0400

    updated PatientDetailsPage with a QuePopup, + QuePopup LINK

--- ReactDjango_JWT_StarterCode-main/frontend/src/pages/PatientDashboardPage/PatientDashboardPage.jsx
+++ ReactDjango_JWT_StarterCode-main/frontend/src/pages/PatientDashboardPage/PatientDashboardPage.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import './PatientDashboardPage.css';
+import QueuePopup from "../../components/QueuePage/QueuePage";
+
 
 const PatientDashboardPage = () => {
+    const [isQueuePopupOpen, setIsQueuePopupOpen] = useState(false);
+
+  // Function to open the QueuePopup
+  const openQueuePopup = () => {
+    setIsQueuePopupOpen(true);
+  };
+
+  // Function to close the QueuePopup
+  const closeQueuePopup = () => {
+    setIsQueuePopupOpen(false);
+  };
+
+
+
   return (
     <div className="patient-dashboard-container">
       <h2>Welcome to Your Patient Dashboard</h2>
@@ -13,6 +30,14 @@ const PatientDashboardPage = () => {
       <Link to="/add-rideshare" className="add-info-button">
         Add Rideshare
       </Link>
+      {/* Button to open the QueuePopup */}
+      <button onClick={openQueuePopup} className="view-queue-button">
+          View Queue
+        </button>
+
+        {/* Render the QueuePopup when isQueuePopupOpen is true */}
+        {isQueuePopupOpen && <QueuePopup onClose={closeQueuePopup} />}
+      
     </div>
   );
 };
