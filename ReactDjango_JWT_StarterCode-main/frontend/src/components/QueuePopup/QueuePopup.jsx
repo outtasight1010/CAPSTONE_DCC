401d80c057588a12626671c86584b85f22f85d97
Michelle Lamelza <mishlamelza@gmail.com>
2023-09-24 02:40:44 -0400

    updated QueuePopup with {patient.name}

--- ReactDjango_JWT_StarterCode-main/frontend/src/components/QueuePopup/QueuePopup.jsx
+++ ReactDjango_JWT_StarterCode-main/frontend/src/components/QueuePopup/QueuePopup.jsx
@@ -2,16 +2,24 @@ import React from "react";
 import './QueuePopup.css';
 
 const QueuePopup = ({ onClose }) => {
+  // Array of patient names
+  const patientNames = ["Teaqula", "Michelle", "Laura", "Erin", "Vincent"];
+
+  // Randomly select a patient name
+  const randomName = patientNames[Math.floor(Math.random() * patientNames.length)];
+
   // This will generate a random queue position (for demonstration)
   const queuePosition = Math.floor(Math.random() * 50) + 1;
 
   return (
     <div className="queue-popup">
       <h3>Your Queue Position:</h3>
-      <p>Welcome to the waiting room! You are number:{queuePosition} in line</p>
+      <p>Welcome to the waiting room! You are number {queuePosition} in line.</p>
+      <p>Patient: {randomName}</p>
       <button onClick={onClose}>Close</button>
     </div>
   );
 };
 
 export default QueuePopup;
+
